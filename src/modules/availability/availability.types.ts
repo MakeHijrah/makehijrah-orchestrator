088f45dc24b4f48c361e3bb8c08e7d4ea1771134
MakeHijrah <info@makehijrah.com>
2026-07-20 11:21:37 +0300

    Add availability domain types

---
+++ src/modules/availability/availability.types.ts
@@ -0,0 +1,21 @@
+export type AvailabilitySlot = {
+  start_at: string;
+  end_at: string;
+};
+
+export type WorkingHoursInterval = {
+  start: string;
+  end: string;
+};
+
+export type WeeklyWorkingHours = Record<
+  string,
+  WorkingHoursInterval[]
+>;
+
+export type AvailabilityResult = {
+  consultant_id: string;
+  slots: AvailabilitySlot[];
+  generated_at: string;
+  cache_ttl_seconds: 120;
+};
